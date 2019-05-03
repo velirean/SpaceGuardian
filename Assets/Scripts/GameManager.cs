c8465530ffa9f5dcac3eef1a141b3275a636ebe6
velirean <47877809+velirean@users.noreply.github.com>
2019-05-03 16:49:44 -0500

    The moon's shield moves around the moon
    Also the welcome text appears before starting the game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+
+    private float startDelay = 1f;
+    private Text mainText;
+    private GameObject mainCanvas;
+    private GameObject gameManager;
+
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager");
+        mainCanvas = GameObject.Find("MainCanvas");
+        mainText = GameObject.Find("MainText").GetComponent<Text>();
+
+        if (instance == null)
+        {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        InitGame();
+    }
+
+    public void InitGame()
+    {
+
+        mainCanvas.SetActive(true);
+        Invoke("HideMainCanvas", startDelay);
+    }
+
+    private void HideMainCanvas()
+    {
+        mainCanvas.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+        mainText.text = "Game Over";
+        mainCanvas.SetActive(true);
+        enabled = false;
+    }
+}
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public Text pointsText;
+    private int points = 0;
+    private int direction = 1;
+    
+
+    public void Die()
+    {
+        GameObject.Find("GameManager").GetComponent<GameManager>().GameOver();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.RotateAround(new Vector3(8, 5), Vector3.forward, 120 * direction * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ChangeDirection();
+        }
+
+        pointsText.text = "Points " + points;
+    }
+
+    private void ChangeDirection()
+    {
+        this.direction *= -1;
+    }
+}
