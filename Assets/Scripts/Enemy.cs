03070339fa599cc76394045b5cf496b0e8011c7e
velirean <47877809+velirean@users.noreply.github.com>
2019-05-03 18:22:01 -0500

    Enemigos con velocidad variable

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
     Vector3 direction;
-    public float speed;
+    private float speed;
 
     private void Start()
     {
+        speed = Random.Range(0.03f, 0.05f);
         direction = (new Vector3(8, 5) - transform.position).normalized;
 
         float rot_z = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
